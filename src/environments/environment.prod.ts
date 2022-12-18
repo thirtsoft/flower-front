aa1bd14d9f2ca8414e9bab58d09cb3a4dfb1a72b
thirdiallo <thirdiallo@gmail.com>
2022-12-18 03:50:21 +0100

    change url access prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+ // apiBaseUrl: 'http://localhost:8081/flowers/v1'
+  apiBaseUrl: 'https://flower-admin.herokuapp.com/flowers/v1'
 };
