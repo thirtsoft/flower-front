8a0943a95010886ae085771410d0c5bca318969c
thirdiallo <thirdiallo@gmail.com>
2022-12-21 01:29:55 +0100

    remove button min() et plus in detail product

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  production: true,
+  production: false,
  // apiBaseUrl: 'http://localhost:8081/flowers/v1'
   apiBaseUrl: 'https://flower-admin.herokuapp.com/flowers/v1'
 };
