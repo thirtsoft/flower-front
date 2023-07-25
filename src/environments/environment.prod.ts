4fd97413650aecee05a40b39100f4be8944ae0f5
asamba <abdoulaye.samba@awaconsulting.com>
2023-07-25 23:18:48 +0000

    update prod env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,4 @@
 export const environment = {
   production: true,
-//  apiBaseUrl: 'https://flower-admin.herokuapp.com/flowers/v1',
   apiBaseUrl: 'https://flowerapi.fleurpourtous.com/flowers/v1'
 };
